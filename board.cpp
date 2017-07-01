2b5b4de74c79993e1df6c66a8016f924b3a0f053
Andrew Guo <andrewguo5@Andrews-MacBook-Pro.local>
2017-07-01 19:36:19 -0400

    board.cpp compiles

--- board.cpp
+++ board.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-#include "board.hpp"
 
-Board::printboard() {
-	//haha wtf
+#define WIDTH 10
+#define HEIGHT 20
+
+class Board {
+private:
+  //Block blocks[WIDTH][HEIGHT];
+  //Piece activePiece;
+
+  int blocks[WIDTH][HEIGHT];
+
+    void printboard();
+
+    int main();
+};
+
+void Board::printboard() {
+	std::cout << "haha wtf";
 }
 
-Board::main() {
+int Board::main() {
 	std::cout << "Hello World\n";
+    return 0;
 }
