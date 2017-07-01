d46ae6a76d723fc0b5792bd85f7a59fa0f69e26f
MBlaureNs <42chrono@gmail.com>
2017-07-01 19:30:34 -0400

    broken board code

---
+++ board.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "board.hpp"
+
+Board::printboard() {
+	//haha wtf
+}
+
+Board::main() {
+	std::cout << "Hello World\n";
+}
---
+++ board.hpp
@@ -0,0 +1,14 @@
+#define WIDTH 10
+#define HEIGHT 20
+
+class Board {
+private:
+  //Block blocks[WIDTH][HEIGHT];
+  //Piece activePiece;
+
+  int blocks[WIDTH][HEIGHT];
+
+	void printboard();
+
+	int main();
+};
