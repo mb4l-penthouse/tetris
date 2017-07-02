a28a71ddad1c3fa4576d63c23dd2360054373b35
Xiao-Ping Huynh <mingebag7@gmail.com>
2017-07-01 20:31:58 -0400

    lol

--- tetromino_pieces.hpp
+++ tetromino_pieces.hpp
@@ -26,3 +26,39 @@ public:
 	TetrominoT();
 	~TetrominoT();
 }
+
+// A row of 3 blocks with 1 block below the right block
+class TetrominoJ : Tetromino {
+public:
+	void fall();
+	void rotate();
+	TetrominoJ();
+	~TetrominoJ();
+}
+
+// A row of 3 blocks with 1 block below the left block
+class TetrominoL : Tetromino {
+public:
+	void fall();
+	void rotate();
+	TetrominoL();
+	~TetrominoL();
+}
+
+// 2 horizontal dominoes with top offset to right
+class TetrominoS : Tetromino {
+public:
+	void fall();
+	void rotate();
+	TetrominoS();
+	~TetrominoS();
+}
+
+// 2 horizontal dominoes with top offset to left
+class TetrominoZ : Tetromino {
+public:
+	void fall();
+	void rotate();
+	TetrominoZ();
+	~TetrominoZ();
+}
